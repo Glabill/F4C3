ba8998dc7089a57046f387b716f17a114bc19e14
Gaston <Gougeon>
2020-02-29 18:04:33 -0500

    Initial Commit

---
+++ F4C3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  F4C3
+//
+//  Created by Gaston Gougeon on 29/02/2020.
+//  Copyright © 2020 com.example. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
